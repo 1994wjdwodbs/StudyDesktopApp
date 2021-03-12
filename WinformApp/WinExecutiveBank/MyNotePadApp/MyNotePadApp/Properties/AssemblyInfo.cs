ed4d28fe900d8b50b7b5771c420bc05f9d9739af
JYJ_FROM_KOOKBI <1994wjdwodbs@gmail.com>
2021-03-12 16:48:36 +0900

    임시 커밋

--- WinformApp/WinExecutiveBank/MyNotePadApp/MyNotePadApp/Properties/AssemblyInfo.cs
+++ WinformApp/WinExecutiveBank/MyNotePadApp/MyNotePadApp/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
 [assembly: AssemblyTitle("MyNotePadApp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("노트패드 ~~ 소ㅑㄹ랴소ㅑㄹ랴~~")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MyNotePadApp")]
