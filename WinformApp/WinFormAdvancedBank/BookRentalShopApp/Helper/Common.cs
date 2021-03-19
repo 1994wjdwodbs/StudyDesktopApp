4760c4033e2f311ea8002b24204748dc2a5fbfab
JYJ_FROM_KOOKBI <1994wjdwodbs@gmail.com>
2021-03-19 10:00:59 +0900

    Commit Again

--- WinformApp/WinFormAdvancedBank/BookRentalShopApp/Helper/Common.cs
+++ WinformApp/WinFormAdvancedBank/BookRentalShopApp/Helper/Common.cs
@@ -25,7 +25,7 @@ namespace BookRentalShopApp.Helper
                     localIP = ip.ToString();
                     break;
                 }
-            }
+            } 
 
             return localIP;
         }
